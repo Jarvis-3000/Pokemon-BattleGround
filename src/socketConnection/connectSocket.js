503aaf8ab582b0df1fbfcd617f7ea6aec493e80d
Satish Gaud <57852939+Jarvis-3000@users.noreply.github.com>
2024-03-20 12:40:24 +0530

    Updated backend url

--- src/socketConnection/connectSocket.js
+++ src/socketConnection/connectSocket.js
@@ -1,7 +1,7 @@
 import React from "react";
 import IO from "socket.io-client";
 
-const URL = "https://pokemonbattlegroundbackend-production.up.railway.app/";
+const URL = "https://pokemon-battleground-backend.onrender.com/";
 // const URL = "http://localhost:5000";
 
 export const socket = IO(URL, {
