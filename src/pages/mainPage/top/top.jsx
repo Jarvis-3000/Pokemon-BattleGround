62861d7661af2b9cbe793d1ed5943b10913295c3
Satish Gaud <57852939+Jarvis-3000@users.noreply.github.com>
2024-09-02 23:31:47 +0530

    Update top.jsx

--- src/pages/mainPage/top/top.jsx
+++ src/pages/mainPage/top/top.jsx
@@ -1,53 +1,58 @@
-import React from "react"
-import { useDispatch, useSelector } from "react-redux"
-import "./top.scss"
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./top.scss";
 
-import {Link} from "react-router-dom"
-import {SocketContext} from "../../../socketConnection/connectSocket"
-import * as gameActions from "../../../redux/gameFunctions/actions"
+import { Link } from "react-router-dom";
+import { SocketContext } from "../../../socketConnection/connectSocket";
+import * as gameActions from "../../../redux/gameFunctions/actions";
 
-function Top(){
-    const [style,setStyle]=React.useState({display:'none'})
-    const [idLoading, setIdLoading] = React.useState(false)
-    const socket=React.useContext(SocketContext)
+function Top() {
+  const [style, setStyle] = React.useState({ display: "none" });
+  const [idLoading, setIdLoading] = React.useState(false);
+  const socket = React.useContext(SocketContext);
 
-    const dispatch=useDispatch()           //useDispatch() for dispatching an action
-    const {groupId} = useSelector(state=>state.gameReducer)
+  const dispatch = useDispatch(); //useDispatch() for dispatching an action
+  const { groupId } = useSelector((state) => state.gameReducer);
 
-    const handleConnectSocket=()=>{
-        setIdLoading(true)
-        
-        socket.emit("start")
-        
-        socket.once("start",({id})=>{
-            setStyle({display:'block'})
-            setIdLoading(false)
-            dispatch(gameActions.setGroupId(id))
-        })
+  const handleConnectSocket = () => {
+    if (!groupId) {
+      setIdLoading(true);
     }
 
-    return(
-        <div className="top">
-            <div className="gameOptions">
-                <button className="startGame" onClick={handleConnectSocket}>Start Game</button>
-                <Link to="/join"><button className="joinGame">Join Game</button></Link>
-            </div>
-            <div className="groupId" style={style}>
-                {idLoading && <span>Id Loading...</span>}
-                {!idLoading && groupId && (
-                    <>
-                        <div className="share">
-                            Share Id
-                        </div>
-                        <div className="id">
-                            {groupId}
-                        </div>
-                    </>
-                )
-            </div>
+    socket.emit("start");
 
-        </div>
-    )
+    socket.once("start", ({ id }) => {
+      setStyle({ display: "block" });
+      setIdLoading(false);
+      dispatch(gameActions.setGroupId(id));
+    });
+  };
+
+  return (
+    <div className="top">
+      <div className="gameOptions">
+        <button className="startGame" onClick={handleConnectSocket}>
+          Start Game
+        </button>
+        <Link to="/join">
+          <button className="joinGame">Join Game</button>
+        </Link>
+      </div>
+      <div className="groupId" style={style}>
+        {idLoading && (
+          <div>
+            <b>Please Wait. Id Loading...</b>
+          </div>
+        )}
+        {!idLoading && groupId && (
+          <>
+            <div className="share">Share Id</div>
+            <div className="id">{groupId}</div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Top
+export default Top;
